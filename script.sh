76142d7984479ecbc5348fc32969a7d5e39a0643
dave-pl <35957391+dave-pl@users.noreply.github.com>
2019-08-16 14:26:29 +0300

    6
    6

--- script.sh
+++ script.sh
@@ -7,12 +7,4 @@ else
 fi
 
 docker stop mytestcontainer
-docker build . -t dave1
-docker run --rm -d -p 85:80 --name mytestcontainer dave1
-if [[ $(curl --silent http://18.216.36.145:85/|grep hello) ]]; then
-    exit 0
-else
-    exit 1
-fi
-
-docker stop mytestcontainer
+docker image rm dave1 nginx
